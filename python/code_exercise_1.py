95ddc1a1c951fbe58298bdcb753350c53a514b35
sheishistoric <estenemily@gmail.com>
2018-06-05 14:16:01 -0400

    added code_exercise_1.py

---
+++ python/code_exercise_1.py
@@ -0,0 +1,6 @@
+name = {"first_name": "Emily", "last_name": "Esten"}
+street = {"house_number": "123", "street_name": "My Street"}
+city_state_zip = {"city": "Providence", "state": "RI", "zip": "02906"}
+print(name["first_name"] + " " + name["last_name"])
+print(street["house_number"] + " " + street["street_name"])
+print(city_state_zip["city"] + "," + " " + city_state_zip["state"] + " " + city_state_zip["zip"])
