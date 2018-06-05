0dc89cdd0568e278ec0ecb6ccfb568abbfcf5b02
sheishistoric <estenemily@gmail.com>
2018-06-05 16:50:39 -0400

    updated branching

---
+++ python/branching.py
@@ -0,0 +1,7 @@
+fruits = ["kiwi", "strawberry", "plum", "lychee"]
+if fruits[0] == "apple":
+    print("Yum!")
+elif fruits[0] == "cardboard" or fruits[0] == "sand":
+    print("Yuck!")
+else:
+    print("Not bad.")
